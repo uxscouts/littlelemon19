c5ccc751edf495566d253737ec684ccca5c7ba1a
UX SCOUTS <admin@uxscouts.com>
2026-04-24 18:54:05 +0000

    added new test

--- littlelemon19/vite.config.mjs
+++ littlelemon19/vite.config.mjs
@@ -1,15 +1,30 @@
+
+
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom', 
+    globals: true,
+  },
+});
+
+/*
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
      test: {
-    environment: 'jsdom', 
+    environment: 'jsdom',
+    globals: true,
   }
 })
+  */
